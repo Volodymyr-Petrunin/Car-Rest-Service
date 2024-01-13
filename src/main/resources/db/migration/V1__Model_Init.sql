965a2af3cacba5ff71d2a7c5924f8533783f786f
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2024-01-13 15:06:53 +0100

    rename tables:)

--- src/main/resources/db/migration/V1__Model_Init.sql
+++ src/main/resources/db/migration/V1__Model_Init.sql
@@ -2,7 +2,7 @@ CREATE SEQUENCE IF NOT EXISTS make_seq START WITH 1 INCREMENT BY 50;
 
 CREATE SEQUENCE IF NOT EXISTS model_seq START WITH 1 INCREMENT BY 50;
 
-CREATE TABLE car
+CREATE TABLE cars
 (
     object_id      VARCHAR(255) NOT NULL,
     car_year       SMALLINT,
@@ -10,20 +10,20 @@ CREATE TABLE car
     CONSTRAINT pk_car PRIMARY KEY (object_id)
 );
 
-CREATE TABLE car_categories
+CREATE TABLE cars_categories
 (
     car_id   VARCHAR(255) NOT NULL,
     category VARCHAR(255)
 );
 
-CREATE TABLE make
+CREATE TABLE makes
 (
     make_id    BIGINT NOT NULL,
     make_model VARCHAR(255),
     CONSTRAINT pk_make PRIMARY KEY (make_id)
 );
 
-CREATE TABLE model
+CREATE TABLE models
 (
     model_id     BIGINT NOT NULL,
     model_name   VARCHAR(255),
@@ -31,11 +31,11 @@ CREATE TABLE model
     CONSTRAINT pk_model PRIMARY KEY (model_id)
 );
 
-ALTER TABLE car
-    ADD CONSTRAINT FK_CAR_ON_MODEL_MODEL FOREIGN KEY (model_model_id) REFERENCES model (model_id);
+ALTER TABLE cars
+    ADD CONSTRAINT FK_CAR_ON_MODEL_MODEL FOREIGN KEY (model_model_id) REFERENCES models (model_id);
 
-ALTER TABLE model
-    ADD CONSTRAINT FK_MODEL_ON_MAKE_MAKE FOREIGN KEY (make_make_id) REFERENCES make (make_id);
+ALTER TABLE models
+    ADD CONSTRAINT FK_MODEL_ON_MAKE_MAKE FOREIGN KEY (make_make_id) REFERENCES makes (make_id);
 
-ALTER TABLE car_categories
-    ADD CONSTRAINT fk_car_categories_on_car FOREIGN KEY (car_id) REFERENCES car (object_id);
+ALTER TABLE cars_categories
+    ADD CONSTRAINT fk_car_categories_on_car FOREIGN KEY (car_id) REFERENCES cars (object_id);
