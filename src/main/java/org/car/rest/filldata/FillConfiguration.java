d2a6ace238af704be22c62a3515fd5eb72bdd669
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2024-01-18 17:51:49 +0100

    fix reader and parser issue

--- src/main/java/org/car/rest/filldata/FillConfiguration.java
+++ src/main/java/org/car/rest/filldata/FillConfiguration.java
@@ -1,35 +1,19 @@
 package org.car.rest.filldata;
 
-import com.opencsv.exceptions.CsvException;
 import org.car.rest.mapper.CarMapper;
 import org.car.rest.parser.CarParser;
-import org.car.rest.reader.ResourceFileReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
-import java.io.IOException;
-import java.util.List;
-
 @Configuration
 @PropertySource("classpath:fill-configuration.properties")
 @Profile("fill-data")
 public class FillConfiguration {
-
     @Bean
-    public ResourceFileReader resourceFileReader(@Value("${filename}") String filename){
-        return new ResourceFileReader(filename);
-    }
-
-    @Bean
-    public List<String[]> csvData(ResourceFileReader reader) throws IOException, CsvException {
-        return reader.read();
-    }
-
-    @Bean
-    public CarParser carParser(List<String[]> csvData, CarMapper carMapper){
-        return new CarParser(csvData, carMapper);
+    public CarParser carParser(@Value("${filename}") String filename, CarMapper carMapper){
+        return new CarParser(filename, carMapper);
     }
 }
--- src/main/java/org/car/rest/parser/CarParser.java
+++ src/main/java/org/car/rest/parser/CarParser.java
@@ -1,25 +1,47 @@
 package org.car.rest.parser;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.car.rest.domain.Car;
 import org.car.rest.mapper.CarMapper;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class CarParser implements Parser<Car> {
-
-    private final List<String[]> csvData;
+    private final String fileName;
     private final CarMapper carMapper;
 
-    public CarParser(List<String[]> csvData, CarMapper carMapper) {
-        this.csvData = csvData;
+    public CarParser(String fileName, CarMapper carMapper) {
+        this.fileName = fileName;
         this.carMapper = carMapper;
     }
 
     @Override
     public Stream<Car> parse() {
-        return csvData.stream()
+        return getCsvData().stream()
                 .skip(1) // start with second element because firs is header
                 .map(carMapper::mapToObject);
+    }
+
+    private List<String[]> getCsvData() {
+        InputStream inputStream = getClass().getResourceAsStream("/" + fileName);
+
+        assert inputStream != null;
+        try (Reader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return read(reader);
+        } catch (IOException e) {
+            throw new RuntimeException("Something wrong in reader!", e);
+        }
+    }
+
+    private List<String[]> read(Reader reader) throws IOException {
+        try (CSVReader csvReader = new CSVReader(reader)) {
+            return csvReader.readAll();
+        } catch (CsvException e) {
+            throw new IOException("Something wrong with cvsReader!", e);
+        }
     }
 }
